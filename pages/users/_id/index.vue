bda6a0717c2ef1ae47c0613507c6a0687e910e4d
oscarMr <oscarhumbertomr@gmail.com>
2019-11-12 12:53:11 -0600

    init

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>This is user's id {{$route.params.id}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        validate(context){
+            return !(/\D/g).test(context.params.id)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Holi</h1>
+        <v-text-field placeholder="user id" v-on:keyup.enter="loadUser" v-model="id"></v-text-field>
+        <v-btn @click="loadUser">Load User</v-btn>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data() {
+            return {
+                id: null
+            }
+        },
+        methods: {
+            loadUser() {
+                this.$router.push(`/users/${this.id}`);
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
