613afa36d46d20b083bf11ad1bf59238dc2298e0
oscar.montejo <oscarhumbertomr@gmail.com>
2019-11-12 18:42:23 -0600

    posts id request

--- components/Card.vue
+++ components/Card.vue
@@ -4,12 +4,13 @@
             Post {{post.id}}
         </v-card-title>
         <v-card-text>
-            <nuxt-link to="`/posts/${post.id}`">{{post.title}}</nuxt-link>
+            <nuxt-link :to="`/posts/${post.id}`">{{post.title}}</nuxt-link>
         </v-card-text>
     </v-card>
 </template>
 
 <script>
+
     export default {
         name: "Card",
         props: {
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
-        {{$route.params.id}}
+        {{post.id}}
+        {{post.title}}
     </div>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "index",
+        asyncData(context){
+            return context.app.$axios.get('https://jsonplaceholder.typicode.com/todos/'+context.params.id).
+            then(response =>{
+                return  {post : response.data}
+            }).catch(error => {
+                console.log(error)
+            })
+        },
+        data() {
+            return {
+                post: {}
+            }
+        },
     }
 </script>
 
