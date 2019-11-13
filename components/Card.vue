20903739bc2d1ef71e56f0ef89adc71f4665bb6b
oscar.montejo <oscarhumbertomr@gmail.com>
2019-11-12 18:20:04 -0600

    card component

---
+++ components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card  nuxt>
+        <v-card-title>
+            Post {{post.id}}
+        </v-card-title>
+        <v-card-text>
+            <nuxt-link to="`/posts/${post.id}`">{{post.title}}</nuxt-link>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            post: {
+                type: Object
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
