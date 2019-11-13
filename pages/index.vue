e6a87d7dade066ad28aa19718f3e46a4f0255345
oscarMr <oscarhumbertomr@gmail.com>
2019-11-13 11:20:24 -0600

    ortografia index ;v

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@
             <v-parallax
                     dark
                     src="/waves.jpg"
+                    style="min-height: 50vh"
             >
                 <v-row
                         align="center"
@@ -31,13 +32,13 @@
             <h1 class="display-1 font-weight-thin mb-4 ">Nuxt:</h1>
         </v-col>
         <v-col cols="12">
-            <h4 class="subheading">Provides de PWA envirioment, do request to Apis and transforms Vue components into HTML ans Js implementing SSR tecnology</h4>
+            <h4 class="subheading">Provides de PWA environment, do request to Apis and transforms Vue components into HTML ans Js implementing SSR technology</h4>
         </v-col>
         <v-col cols="12">
             <h1 class="display-1 font-weight-thin mb-4 ">Vuetify:</h1>
         </v-col>
         <v-col cols="12">
-            <h4 class="subheading">Vuetify is a vuejs toolchaing framework, provides components such as buttons, card, v-paralax ... etc </h4>
+            <h4 class="subheading">Vuetify is a vuejs toolchain framework, provides components such as buttons, card, v-paralax ... etc </h4>
         </v-col>
         <v-col cols="12">
             <h1 class="display-1 font-weight-thin mb-4 ">Laravel:</h1>
@@ -45,9 +46,8 @@
         <v-col cols="12">
             <h4 class="subheading">Provides the backend solution, Laravel is a big framework whit a lot o tools ready to use</h4>
         </v-col>
-
         <v-col cols="12">
-            <h4 class="subheading">This progect still wroging</h4>
+            <h4 class="subheading">This project still wronging</h4>
         </v-col>
 
     </v-row>
--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,41 +1,48 @@
 <template>
-    <div>
-        <v-card
-                class="mx-auto"
-                color="#26c6da"
-                dark
-                max-width="400"
-        >
-            <v-card-title>
-                <v-icon
-                        large
-                        left
-                        class="mr-1"
-                >
-                    mdi-key-variant
-                </v-icon>
-                <span class="title font-weight-light">Post id {{post.id}}</span>
-            </v-card-title>
+    <v-row
+            align="center"
+            justify="center"
+            style="min-height: 80vh"
 
-            <v-card-text class="headline font-weight-bold">
-                {{post.title}}
-            </v-card-text>
-
-            <v-card-actions>
-                <v-list-item class="grow">
-                    <v-row
-                            align="center"
-                            justify="end"
+    >
+        <v-col cols="12" md="6">
+            <v-card
+                    class="mx-auto"
+                    color="#26c6da"
+                    dark
+                    max-width="400"
+            >
+                <v-card-title>
+                    <v-icon
+                            large
+                            left
+                            class="mr-1"
                     >
-                        <v-btn text @click="$router.back()">
-                            <v-icon class="mr-1">mdi-keyboard-return</v-icon>
-                            <span class="subheading mr-2">Regresar</span>
-                        </v-btn>
-                    </v-row>
-                </v-list-item>
-            </v-card-actions>
-        </v-card>
-    </div>
+                        mdi-key-variant
+                    </v-icon>
+                    <span class="title font-weight-light">Post id {{post.id}}</span>
+                </v-card-title>
+
+                <v-card-text class="headline font-weight-bold">
+                    {{post.title}}
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-list-item class="grow">
+                        <v-row
+                                align="center"
+                                justify="end"
+                        >
+                            <v-btn text @click="$router.back()">
+                                <v-icon class="mr-1">mdi-keyboard-return</v-icon>
+                                <span class="subheading mr-2">Regresar</span>
+                            </v-btn>
+                        </v-row>
+                    </v-list-item>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-row>
 </template>
 
 <script>
