5d8d9211831c035c63af61fe6353565e584f05d4
oscarMr <oscarhumbertomr@gmail.com>
2019-11-13 11:37:30 -0600

    fix ortografia

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
             <v-parallax
                     dark
                     src="/waves.jpg"
-                    style="min-height: 50vh"
+                    style="min-height: 50vh; margin-top: 0"
             >
                 <v-row
                         align="center"
@@ -28,8 +28,8 @@
                 </v-row>
             </v-parallax>
         </v-col>
-        <v-col cols="12">
-            <h1 class="display-1 font-weight-thin mb-4 ">Nuxt:</h1>
+        <v-col cols="12" c>
+            <h1 class="display-1 font-weight-thin my-4  ">Nuxt:</h1>
         </v-col>
         <v-col cols="12">
             <h4 class="subheading">Provides de PWA environment, do request to Apis and transforms Vue components into HTML ans Js implementing SSR technology</h4>
@@ -47,7 +47,7 @@
             <h4 class="subheading">Provides the backend solution, Laravel is a big framework whit a lot o tools ready to use</h4>
         </v-col>
         <v-col cols="12">
-            <h4 class="subheading">This project still wronging</h4>
+            <h4 class="subheading">This project still growing</h4>
         </v-col>
 
     </v-row>
