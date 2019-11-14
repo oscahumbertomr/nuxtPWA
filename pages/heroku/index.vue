032ba3a58ce50db3baa79c39a5cd111512a92334
oscarMr <oscarhumbertomr@gmail.com>
2019-11-14 09:07:23 -0600

    skeleton animate on heroku response

--- pages/heroku/index.vue
+++ pages/heroku/index.vue
@@ -1,8 +1,27 @@
 <template>
-    <div>
-        <h1>Heroku test:</h1>
-        <h2>Result = {{test}}</h2>
-    </div>
+    <v-div>
+        <v-row>
+            <v-col cols="12">
+                <h1>Heroku test:</h1>
+            </v-col>
+        </v-row>
+        <v-row v-if="test">
+            <v-col cols="12">
+                <h2>Result = {{test}}</h2>
+            </v-col>
+        </v-row>
+        <v-row v-else>
+            <v-col cols="auto">
+                <h2>Result =</h2>
+            </v-col>
+            <v-col cols="3" class="pl-0 ">
+                <v-skeleton-loader
+                        style="height: 20px;margin-top: 8px;padding-left: 0"
+                        type="text"
+                ></v-skeleton-loader>
+            </v-col>
+        </v-row>
+    </v-div>
 </template>
 
 <script>
@@ -15,7 +34,7 @@
         },
         methods: {
             testHerokuApi() {
-                this.$axios.get('https://nuxt-pwa-oscar.herokuapp.com/api/test').then(response=>{
+                this.$axios.get('https://nuxt-pwa-oscar.herokuapp.com/api/test').then(response => {
                     console.log(response)
                     this.test = response.data
                 });
