77a32e97909c98e108832221fa25a29fb95d72f7
oscarMr <oscarhumbertomr@gmail.com>
2019-11-29 15:34:12 -0600

    fix auth/user throuble

--- plugins/axiosConfig.js
+++ plugins/axiosConfig.js
@@ -4,7 +4,8 @@ export default function ({ $axios, redirect, store,route }) {
         const code = parseInt(error.response && error.response.status)
         // console.log(error)
         window.erplug = error
-        if (code === 401) {
+        if (code === 401 && !/api\/auth\/user/.test(error.config.url)) {
+            console.log('401')
             // urlTriedToVisit = item.to
             store.dispatch('setUrlTriedToVisit', route.path)
             store.dispatch('setModalLoginStatus', true)
